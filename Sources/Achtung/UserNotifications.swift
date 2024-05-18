a687833cf5db0cbea18f47481ac98895c05ebb86
Ben Gottlieb <ben@standalone.com>
2024-05-18 05:12:11 -0500

    added UserNotifications.setBadge(count:)

--- Sources/Achtung/UserNotifications.swift
+++ Sources/Achtung/UserNotifications.swift
@@ -10,7 +10,7 @@
 import UserNotifications
 
 
-@available(iOS 15.0, *)
+@available(iOS 16.0, *)
 public actor AchtungNotifications: NSObject {
 	public static let instance = AchtungNotifications()
 	
@@ -21,10 +21,26 @@ public actor AchtungNotifications: NSObject {
 		UNUserNotificationCenter.current().delegate = self
 	}
 	
-	@discardableResult public func playSound(named: String, at date: Date = .now) -> String {
-		let trigger = UNCalendarNotificationTrigger(dateMatching: Calendar.current.dateComponents([.day, .month, .year, .hour, .minute, .second, .nanosecond], from: date.addingTimeInterval(0.01)), repeats: false)
+	@discardableResult public func playSound(named: String, at date: Date? = nil) -> String {
+		let triggerDate = date ?? .now
+		let trigger = UNCalendarNotificationTrigger(dateMatching: Calendar.current.dateComponents([.day, .month, .year, .hour, .minute, .second, .nanosecond], from: triggerDate.addingTimeInterval(0.01)), repeats: false)
 		let content = UNMutableNotificationContent()
 		content.sound = UNNotificationSound(named: UNNotificationSoundName(named))
+		let id = UUID().uuidString
+		let request = UNNotificationRequest(identifier: id, content: content, trigger: trigger)
+		
+		UNUserNotificationCenter.current().add(request)
+		return id
+	}
+	
+	@discardableResult public func setBadge(count: Int, at date: Date? = nil) -> String? {
+		guard let date else {
+			UNUserNotificationCenter.current().setBadgeCount(count)
+			return nil
+		}
+		let trigger = UNCalendarNotificationTrigger(dateMatching: Calendar.current.dateComponents([.day, .month, .year, .hour, .minute, .second, .nanosecond], from: date.addingTimeInterval(0.01)), repeats: false)
+		let content = UNMutableNotificationContent()
+		content.badge = NSNumber(value: count)
 		let id = UUID().uuidString
 		let request = UNNotificationRequest(identifier: id, content: content, trigger: trigger)
 		
@@ -37,7 +53,7 @@ public actor AchtungNotifications: NSObject {
 	}
 }
 
-@available(iOS 15.0, *)
+@available(iOS 16.0, *)
 extension AchtungNotifications: UNUserNotificationCenterDelegate {
 	public func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification) async -> UNNotificationPresentationOptions {
 		[.banner, .badge, .sound]
